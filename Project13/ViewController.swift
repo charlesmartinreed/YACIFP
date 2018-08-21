e8ad810743d74d836424407f1267fc5c63c5ef76
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-08-21 16:59:12 -0500

    Complete version. Works using photo library, camera not enabled as this is not a production app.

--- Project13/ViewController.swift
+++ Project13/ViewController.swift
@@ -7,28 +7,137 @@
 //
 
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var intensity: UISlider!
     
+    //we'll use this to store the image that the user selected
+    var currentImage: UIImage!
+    
+    //CoreImage properties
+    //CIContext handles rendering
+    var context: CIContext!
+    
+    //stores the user's filter; we'll handle the inputs elsewhere
+    var currentFilter: CIFilter!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //view controller title - title stands for Yet Another Core Image Filters Program, per Paul haha.
+        title = "YACIFP"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else { return }
+        
+        dismiss(animated: true, completion: nil)
+        
+        //we need to get the original image, before filtering, to facilated swapping filters as the user cycles through them
+        currentImage = image
+        
+        //let users controller the amount of the filtering effect
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        //call our applyProcessing function
+        applyProcessing()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
     @IBAction func changeFilter(_ sender: Any) {
+        
+        //create our actionsheet to let the user choose their filtr
+        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+        
+        //add the filter options to the alert controller and use our setFilter() as a completion handler
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        present(ac, animated: true, completion: nil)
     }
     
+    //MARK: - Functions for saving the filtered image to Photos Album
     @IBAction func save(_ sender: Any) {
+        //MARK: - UIImageWriteToSavedPhotosAlbum
+        UIImageWriteToSavedPhotosAlbum(imageView.image!, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+        
+    }
+    //Error? because we may not get an error - in that case, it'll be nil.
+    //if we do get an error, we unwrap and use the localizedDescription to describe the issue to users
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(ac, animated: true, completion: nil)
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your masterpiece has been saved to the photo album", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(ac, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func setFilter(action: UIAlertAction) {
+        //make sure we have a valid image before attempting processing
+        guard currentImage != nil else { return }
+        
+        //set the filter type to the one indicated by the user's selection
+        currentFilter = CIFilter(name: action.title!)
+        
+        //grab our original, unfiltered image
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        //apply our user's chosen filter
+        applyProcessing()
+        
+    }
+    
+    func applyProcessing() {
+        
+        //we're going to check that a particular filter value has a set of input keys and, if so, use them
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)}
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey)}
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey)}
+        if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey)}
+        
+        //extent specifies a rect that encompasses the outputImage, i.e., create a new filtered image out of ALL of the last image
+        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+            let processedImage = UIImage(cgImage: cgimg)
+            imageView.image = processedImage
+        }
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
+        
+        applyProcessing()
     }
     
 
