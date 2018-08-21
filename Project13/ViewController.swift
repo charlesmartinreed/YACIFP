531ce1ab2080af545a417051a4d08d63189411ee
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-08-21 15:37:05 -0500

    Initial Commit

---
+++ Project13/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Charles Martin Reed on 8/21/18.
+//  Copyright © 2018 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+
+}
+
